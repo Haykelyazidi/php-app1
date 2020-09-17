c56cdad030d37c7ee1cb6ac22d061bd8a9801926
Dwija Vivek <dwija.learning@gmail.com>
2020-09-17 17:15:27 +0530

    App Deployment Dockerfile added

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM php:7.3-apache 
-ADD projCert/website /var/www/html
+FROM php:7.0-apache
+RUN apt-get update
+COPY projCert/website /var/www/html
 CMD apachectl -D FOREGROUND
---
+++ delete-app.sh
@@ -0,0 +1,11 @@
+echo "Remove Existing Docker PHP App"
+for i in `docker ps -a| grep edureka-app|awk '{print $1}'`
+do 
+	docker stop $i
+	docker rm $i
+done
+echo "Remove Existing Docker PHP App Image"
+for i in `docker images | grep edureka-app | awk '{print $3}'`
+do
+	docker rmi $i
+done
